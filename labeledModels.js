14dea03b54d84497ef18464338938c8963deb235
Gerardo Saucedo <jegsg@hotmail.es>
2019-08-23 14:21:53 -0500

    Update labeledModels.js

--- labeledModels.js
+++ labeledModels.js
@@ -1,4 +1,7 @@
+import * as descriptors from 'raw.githubusercontent.com/GeraSaucedo/testingMod/master/descriptorModels'
+
+
 labeledFaceDescriptors_models = [
-    new faceapi.LabeledFaceDescriptors('Black Widow', descriptorsBlackWidow),
-    new faceapi.LabeledFaceDescriptors('Thor', descriptorsThor),
+    new faceapi.LabeledFaceDescriptors('Black Widow', descriptors.descriptorsBlackWidow),
+    new faceapi.LabeledFaceDescriptors('Thor', descriptors.descriptorsThor),
 ] 
